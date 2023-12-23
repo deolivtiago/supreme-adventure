81ad64bf214a4cc360d9abf81aa6e4a798631ecb
Tiago de Oliveira <deoliv.tiago@gmail.com>
2023-12-23 11:16:22 -0300

    chore: init spring boot app

---
+++ src/main/java/io/clarx/ellionapi/EllionAPIApplication.java
@@ -0,0 +1,13 @@
+package io.clarx.ellionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EllionAPIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EllionAPIApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/clarx/ellionapi/EllionAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package io.clarx.ellionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EllionAPIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
